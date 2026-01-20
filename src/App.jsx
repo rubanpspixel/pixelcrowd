93b22e9d38115e0ccaa97a4f67148bc846e401ca
rubanpspixel <rubanps@gmail.com>
2026-01-20 14:22:59 +0530

    Remove unnecessary blank lines in App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,8 @@ import { useState } from 'react'
 import './App.scss'
 import Header from "./components/Header";
 import HeroBanner from "./components/herobanner";
-
-
 function App() {
   const [count, setCount] = useState(0)
-
   return (           
        <>
     <Header />
