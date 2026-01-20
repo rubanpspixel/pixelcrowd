1d9277d726aab7e1611fb506ea51ceae7bf4d616
Senthil Pournapandi <senthil.pournapandi@kgisl.com>
2026-01-20 14:21:13 +0530

    Fix Vite base path for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import './App.scss'
 import Header from "./components/Header";
-import Hero from './components/herobanner';
+import HeroBanner from "./components/herobanner";
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +11,7 @@ function App() {
        <>
     <Header />
        <main>
-        <Hero />
+        <HeroBanner />
        </main>
     </>
   )
