ce4d671eb3f829bc6388d7f7b3938e60981e89b5
Senthil Pournapandi <senthil.pournapandi@kgisl.com>
2026-01-20 12:29:32 +0530

    Fix Vite base path for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,11 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-<<<<<<< HEAD
+    
        
       <h1>Coming Soon</h1>
        
-=======
-     <h1>Coming Soon</h1>
->>>>>>> 4bc17cdc77c1654d5b894da35f82ef9e1321cd57
-    </>
+     
   )
 }
 
